1f375e7305e0dbd528f0d0a65be178c0bcedcf02
Huang Chaohui <1466825121@qq.com>
2019-06-02 11:53:28 +0800

    "可以调用摄像头拍照"

--- app/src/main/java/com/example/hui/testcamera/MainActivity.java
+++ app/src/main/java/com/example/hui/testcamera/MainActivity.java
@@ -1,20 +1,25 @@
 package com.example.hui.testcamera;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.graphics.Rect;
 import android.net.Uri;
 import android.os.Build;
 import android.provider.MediaStore;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
@@ -28,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button takePhoto = (Button) findViewById(R.id.take_photo);
+        picture = findViewById(R.id.picture);
         takePhoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -61,20 +67,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         switch (requestCode){
             case TAKE_PHOTO:
-                if(requestCode==RESULT_OK){
+                if(resultCode == RESULT_OK){
+                    Bitmap bitmap = null;
                     try {
-                        Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri));
-                        if (bitmap!=null){
-                            Log.i("TAG","图片存在");
-                        }
-                        picture.setImageBitmap(bitmap);
-                    }catch (IOException e){
+                        bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri));
+                    } catch (FileNotFoundException e) {
                         e.printStackTrace();
                     }
+                    picture.setImageBitmap(bitmap);
                 }
                 break;
              default:
                  break;
         }
     }
+
+
 }
