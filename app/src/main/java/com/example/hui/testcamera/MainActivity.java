0980cebce1bf806719a46d476710066ab2cfcff9
Huang Chaohui <1466825121@qq.com>
2019-06-22 15:52:40 +0800

    1.添加java8支持 2.添加播放视频功能

--- app/src/main/java/com/example/hui/testcamera/MainActivity.java
+++ app/src/main/java/com/example/hui/testcamera/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.hui.testcamera;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
@@ -8,6 +9,7 @@ import android.graphics.Matrix;
 import android.graphics.Rect;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
@@ -17,16 +19,24 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.qw.soul.permission.SoulPermission;
+import com.qw.soul.permission.bean.Permission;
+import com.qw.soul.permission.callbcak.CheckRequestPermissionListener;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+
+import cn.jzvd.JzvdStd;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final int TAKE_PHOTO = 1;
     private ImageView picture;
     private Uri imageUri;
+    JzvdStd jzvdStd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -59,6 +69,21 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(intent,TAKE_PHOTO);
 
 
+            }
+        });
+        jzvdStd = (JzvdStd) findViewById(R.id.jz_video);
+        SoulPermission.getInstance().checkAndRequestPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE, new CheckRequestPermissionListener() {
+            @Override
+            public void onPermissionOk(Permission permission) {
+                String path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/DCIM/Camera/VID_20190622_115322.mp4";
+                jzvdStd.setUp(path
+                        , "饺子快长大");
+            }
+
+            @Override
+            public void onPermissionDenied(Permission permission) {
+                Toast.makeText(MainActivity.this, permission.toString() +
+                        " \n is refused you can not do next things", Toast.LENGTH_SHORT).show();
             }
         });
     }
