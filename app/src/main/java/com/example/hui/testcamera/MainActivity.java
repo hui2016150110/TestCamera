92a1c9ee64309f6d89d83eadd7a3257da7d58e8a
Huang Chaohui <1466825121@qq.com>
2019-06-23 10:36:34 +0800

    "可以调用系统自带的播放器播放视频"

--- app/src/main/java/com/example/hui/testcamera/MainActivity.java
+++ app/src/main/java/com/example/hui/testcamera/MainActivity.java
@@ -16,6 +16,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -23,12 +24,16 @@ import android.widget.Toast;
 
 import com.qw.soul.permission.SoulPermission;
 import com.qw.soul.permission.bean.Permission;
+import com.qw.soul.permission.bean.Permissions;
 import com.qw.soul.permission.callbcak.CheckRequestPermissionListener;
+import com.qw.soul.permission.callbcak.CheckRequestPermissionsListener;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+import cn.jzvd.JZMediaInterface;
+import cn.jzvd.Jzvd;
 import cn.jzvd.JzvdStd;
 
 public class MainActivity extends AppCompatActivity {
@@ -37,13 +42,26 @@ public class MainActivity extends AppCompatActivity {
     private ImageView picture;
     private Uri imageUri;
     JzvdStd jzvdStd;
+    private Button videoButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        String path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/DCIM/Camera/VID_20190622_115322.mp4";
         Button takePhoto = (Button) findViewById(R.id.take_photo);
         picture = findViewById(R.id.picture);
+        videoButton = findViewById(R.id.videoButton);
+        videoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                File file = new File(path);
+                Uri uri = Uri.parse(path);
+                intent.setDataAndType(uri, "video/*");
+                startActivity(intent);
+            }
+        });
+
         takePhoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -72,18 +90,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         jzvdStd = (JzvdStd) findViewById(R.id.jz_video);
-        SoulPermission.getInstance().checkAndRequestPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE, new CheckRequestPermissionListener() {
+        SoulPermission.getInstance().checkAndRequestPermissions(Permissions.build(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE), new CheckRequestPermissionsListener() {
             @Override
-            public void onPermissionOk(Permission permission) {
-                String path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/DCIM/Camera/VID_20190622_115322.mp4";
-                jzvdStd.setUp(path
-                        , "饺子快长大");
+            public void onAllPermissionOk(Permission[] allPermissions) {
+                jzvdStd.setUp(path,"视频播放");
             }
 
             @Override
-            public void onPermissionDenied(Permission permission) {
-                Toast.makeText(MainActivity.this, permission.toString() +
-                        " \n is refused you can not do next things", Toast.LENGTH_SHORT).show();
+            public void onPermissionDenied(Permission[] refusedPermissions) {
+
             }
         });
     }
@@ -107,5 +122,17 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private long getVideoLong(String path){
+
+        return 0;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (Jzvd.backPress()) {
+            return;
+        }
+        super.onBackPressed();
+    }
 
 }
